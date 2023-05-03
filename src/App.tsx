7a3381c6b8932f62e96a4149819ab9ad4e84c2af
Oleh Khalanskyi <okhalanskyi@gmail.com>
2023-05-03 23:38:50 +0300

    commit eighth

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
            Khalanskyi
-        Ivanov
+          Ivanov
            Dmytrinskyi
       </header>
     </div>
